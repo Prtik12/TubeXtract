736744727fb9d55d4180788d57d6a74f302c1b58
babyJesus12 <babyjesus7695@gmail.com>
2025-02-23 10:18:37 +0530

    added error page

---
+++ app/not-found.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import FuzzyText from "@/components/ui/FuzzyText/FuzzyText";
+
+
+export default function NotFoundPage() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-950 text-white">
+      <div className="relative w-full h-[150px] flex items-center justify-center">
+        <FuzzyText fontSize={"6rem"} fontWeight={900} color="#fff">
+          Error 404
+        </FuzzyText>
+      </div>
+    </div>
+  );
+}
